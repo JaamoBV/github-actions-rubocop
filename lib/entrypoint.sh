cf834f01aa6d28901bae725e5be5f9c01271fccd
Daniel D <danieldelnaaij@jaamo.nl>
2022-01-24 17:30:09 +0100

    Update gem versions

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-gem install rubocop -v 0.80.1
-gem install rubocop-rspec -v 1.38.1
-gem install rubocop-performance -v 1.5.2
-gem install rubocop-rails -v 2.4.2
+gem install rubocop -v 1.20
+gem install rubocop-rspec -v 2.4.0
+gem install rubocop-performance -v 1.11.5
+gem install rubocop-rails -v 2.12.2
+gem install standard -v 1.3.0
 
 ruby /action/lib/index.rb
